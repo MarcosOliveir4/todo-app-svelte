09014f23188322c1c54891b7ad17b1bdf9a0ca33
MarcosOliveir4 <marcosassis071@gmail.com>
2024-03-09 09:32:34 -0300

    feat: to-do-list com daisyUI

---
+++ src/components/List.svelte
@@ -0,0 +1,50 @@
+<script lang="ts">
+	type Tarefa = {
+		id: number;
+		nome: string;
+		checked: boolean;
+	};
+
+	export let tarefas: Tarefa[];
+
+	function removeTarefa(index: number) {
+		tarefas.splice(index, 1);
+		tarefas = [...tarefas];
+	}
+</script>
+
+<section class="bg-slate-200 p-5 rounded-lg shadow-md">
+	{#if tarefas.length === 0}
+		<p class="text-center">Nenhuma tarefa cadastrada</p>
+	{/if}
+	<ul class="flex flex-col">
+		{#each tarefas as { id, nome, checked }, i}
+			<li class="flex justify-between">
+				<input type="checkbox" class="checkbox checkbox-success" bind:checked />
+				<span class="text-lg {checked ? 'line-through' : 'no-underline'}">
+					{nome}
+				</span>
+				<button
+					class="btn btn-sm btn-circle btn-outline btn-error"
+					on:click={() => removeTarefa(i)}
+				>
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						class="h-6 w-6"
+						fill="none"
+						viewBox="0 0 24 24"
+						stroke="currentColor"
+					>
+						<path
+							stroke-linecap="round"
+							stroke-linejoin="round"
+							stroke-width="2"
+							d="M6 18L18 6M6 6l12 12"
+						/>
+					</svg>
+				</button>
+			</li>
+			<span class="divider"></span>
+		{/each}
+	</ul>
+</section>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,53 +1,47 @@
-<script>
-	import Header from './Header.svelte';
-	import './styles.css';
+<script lang="ts">
+	import 'tailwindcss/tailwind.css';
+	import List from '../components/List.svelte';
+	type Tarefa = {
+		id: number;
+		nome: string;
+		checked: boolean;
+	};
+
+	let tarefa: Tarefa[] = [];
+	let value = '';
+	$: disableButton = value === '';
+
+	function addTarefa() {
+		tarefa = [...tarefa, { id: tarefa.length + 1, nome: value, checked: false }];
+		value = '';
+	}
 </script>
 
-<div class="app">
-	<Header />
-
-	<main>
+<div class="app pt-10">
+	<main class="container">
 		<slot />
+		<section class="flex flex-col gap-10">
+			<form
+				class="bg-slate-200 rounded-lg p-5 shadow-md flex gap-5"
+				on:submit|preventDefault={addTarefa}
+			>
+				<input
+					bind:value
+					type="text"
+					placeholder="Digite sua tarefa..."
+					class="input input-bordered w-full max-w-xs"
+				/>
+				<button class="btn" disabled={disableButton}>ADD Tarefa</button>
+			</form>
+			<List tarefas={tarefa} />
+		</section>
 	</main>
-
-	<footer>
-		<p>visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to learn SvelteKit</p>
-	</footer>
 </div>
 
 <style>
 	.app {
 		display: flex;
-		flex-direction: column;
-		min-height: 100vh;
-	}
-
-	main {
-		flex: 1;
-		display: flex;
-		flex-direction: column;
-		padding: 1rem;
-		width: 100%;
-		max-width: 64rem;
-		margin: 0 auto;
-		box-sizing: border-box;
-	}
-
-	footer {
-		display: flex;
-		flex-direction: column;
 		justify-content: center;
-		align-items: center;
-		padding: 12px;
-	}
-
-	footer a {
-		font-weight: bold;
-	}
-
-	@media (min-width: 480px) {
-		footer {
-			padding: 12px 0;
-		}
+		height: 100vh;
 	}
 </style>
